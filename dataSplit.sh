4287dbf9855cc0555013acadb203b5fc5bc240d3
Ahmed Amin <ahmedredaselim@gmail.com>
2018-09-04 18:40:12 +0200

    added data splitter

--- dataSplit.sh
+++ dataSplit.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-cd dataset_RGB_processed_binary
+cd $1
 X=$(ls -1q | wc -l)
 TEST_PORTION=0.3 		# max value = 1
 TEST_SPLIT=$(bc -l <<< "$X*$TEST_PORTION")
 TEST_SPLIT=$( printf "%.0f" $TEST_SPLIT)
-
+mkdir test_split train_split
+########
+i=$TEST_SPLIT
+for filename in ./*.jpg; do
+	mv $filename test_split/
+	((i--))
+	if [ $i == 0 ]; then
+		break
+	fi
+done
+########
+for filename in ./*.jpg; do
+	mv $filename train_split/
+done
 
